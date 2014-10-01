fce0e2938063f5d20f4268c60f10a2cc2bb9dc47
Shrinath <shri.dhatrak123@gmail.com>
2014-10-01 15:26:29 +0530

    Added Shrinath.Java

---
+++ Shrinath.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Shrinath{
+
+	public static void main(String [] args) throws Exception{
+		Scanner input = new Scanner(System.in);
+		int str = input.nextInt();
+		System.out.println("New "+ str);
+		input.close();
+	}
+}
