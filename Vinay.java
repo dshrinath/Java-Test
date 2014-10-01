abb7b77c0ce5b96c517d0103d4108eb9b68d35f1
Vinay Jain <BBI@Vinays-MacBook-Pro.local>
2014-10-01 14:28:54 +0530

    Training test

---
+++ Vinay.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Vinay{
+
+	public static void main(String [] args) throws Exception{
+		Scanner input = new Scanner(System.in);
+		int str = input.nextInt();
+		System.out.println("Hello "+ str);
+		input.close();
+	}
+}
